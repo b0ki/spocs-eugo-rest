6d42f66214e8d50533e373c85f83cf8bddf8edd0
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-04-02 09:37:28 +0200

    Make one-time code static

--- src/main/java/si/gov/eugo/service/EugoServiceFactory.java
+++ src/main/java/si/gov/eugo/service/EugoServiceFactory.java
@@ -3,6 +3,8 @@ package si.gov.eugo.service;
 import org.jboss.resteasy.client.ProxyFactory;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,22 +15,33 @@ import java.util.Properties;
  * @created 3/27/13 4:19 PM
  */
 public class EugoServiceFactory {
+    private static final Logger log = LoggerFactory.getLogger(EugoServiceFactory.class);
 
-    public static EugoService createEugoService() throws IOException {
-        Properties defaults = new Properties();
-        defaults.load(EugoServiceFactory.class.getResourceAsStream("eugo-config-default.properties"));
+    private static final String apiEndpointUrl;
+    private static final Properties properties;
 
+    static {
+        try {
+            Properties defaults = new Properties();
+            defaults.load(EugoServiceFactory.class.getResourceAsStream("eugo-config-default.properties"));
 
-        Properties properties = new Properties(defaults);
-        InputStream propertiesSrc = EugoServiceFactory.class.getResourceAsStream("/eugo-config.properties");
-        if (propertiesSrc != null) {
-            properties.load(propertiesSrc);
+            Properties props = new Properties(defaults);
+            InputStream propertiesSrc = EugoServiceFactory.class.getResourceAsStream("/eugo-config.properties");
+            if (propertiesSrc != null) {
+                props.load(propertiesSrc);
+            }
+            properties = props;
+            apiEndpointUrl = properties.getProperty("api.endpoint");
+            log.debug("Using properties: {}", properties);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading EUGO properties.", e);
         }
-        String apiEndpoint = properties.getProperty("api.endpoint");
 
         ResteasyProviderFactory factory = ResteasyProviderFactory.getInstance();
         RegisterBuiltin.register(factory);
+    }
 
-        return ProxyFactory.create(EugoService.class, apiEndpoint);
+    public static EugoService createEugoService() throws IOException {
+        return ProxyFactory.create(EugoService.class, apiEndpointUrl);
     }
 }
